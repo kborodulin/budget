c879b90684ce2db62175b8c13081ea53ef3a0e63
ekaterina belolipetskaya <ia.kuzzia@gmail.com>
2020-01-22 14:00:42 +0300

    FBCF-014: Service for registration was added. DAO classes was added as mock.

--- src/main/java/ru/innopolis/service/RegServiceImpl.java
+++ src/main/java/ru/innopolis/service/RegServiceImpl.java
@@ -1,7 +1,11 @@
 package ru.innopolis.service;
 
 import org.springframework.stereotype.Service;
+import ru.innopolis.db.dao.UserDao;
+import ru.innopolis.db.model.User;
 import ru.innopolis.service.dto.UserDto;
+
+import java.util.UUID;
 
 /**
  * RegServiceImpl
@@ -10,7 +14,16 @@ import ru.innopolis.service.dto.UserDto;
  */
 @Service
 public class RegServiceImpl implements RegService {
-    public void registerUser(UserDto userDto) {
+    private UserDao userDao;
 
+    public RegServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public UserDto registerUser(UserDto userDto) {
+        User user = new User(UUID.randomUUID(), userDto.getUsername(), userDto.getPassword(), userDto.getEmail());
+        userDao.addUser(user);
+        userDto.setId(user.getId());
+        return userDto;
     }
 }
--- src/main/java/ru/innopolis/service/dto/UserDto.java
+++ src/main/java/ru/innopolis/service/dto/UserDto.java
@@ -1,9 +1,92 @@
 package ru.innopolis.service.dto;
 
+import java.util.Objects;
+import java.util.UUID;
+
 /**
  * UserDto
  *
  * @author Ekaterina Belolipetskaya
  */
 public class UserDto {
+    private UUID id;
+    private String username;
+    private String password;
+    private String repeatPassword;
+    private String email;
+
+    public UserDto() {
+    }
+
+    public UserDto(UUID id, String username, String password, String repeatPassword, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.repeatPassword = repeatPassword;
+        this.email = email;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRepeatPassword() {
+        return repeatPassword;
+    }
+
+    public void setRepeatPassword(String repeatPassword) {
+        this.repeatPassword = repeatPassword;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UserDto)) return false;
+        UserDto userDto = (UserDto) o;
+        return Objects.equals(id, userDto.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "UserDto{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", repeatPassword='" + repeatPassword + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
