b443eec26eee4a49022d58c6064cc49360b363d3
ekaterina belolipetskaya <ia.kuzzia@gmail.com>
2020-01-25 14:32:01 +0300

    FBC-002: Fix ConnectionDB: drivers loading and logging were added. Folder log was added in .gitignore.

--- src/main/java/ru/innopolis/db/dao/ConnectionDB.java
+++ src/main/java/ru/innopolis/db/dao/ConnectionDB.java
@@ -1,14 +1,28 @@
 package ru.innopolis.db.dao;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionDB {
-    public static Connection connectDB() throws SQLException {
-        String url = "jdbc:postgresql://localhost:5432/budget";
-        String user = "postgres";
-        String password = "postgres";
-        return DriverManager.getConnection(url, user, password);
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConnectionDB.class);
+    private static final String POSTGRES_DRIVER = "org.postgresql.Driver";
+    public static final String POSTGRES_URL_USERS = "jdbc:postgresql://localhost:5432/budget";
+    public static final String POSTGRES_USER = "postgres";
+    public static final String POSTGRES_PASSWORD = "postgres";
+
+    public static Connection connectDB(){
+        Connection connection = null;
+        try {
+            LOGGER.debug("getting connection");
+            Class.forName(POSTGRES_DRIVER);
+            connection = DriverManager.getConnection(POSTGRES_URL_USERS, POSTGRES_USER, POSTGRES_PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            LOGGER.error("error during getting connection", e);
+        }
+        return connection;
     }
 }
