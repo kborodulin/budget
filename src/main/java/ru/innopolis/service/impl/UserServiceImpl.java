1b508a1176b2789e3aa27197d17b651d6da7c0a6
Borodulin Kirill <kborodulin@diasoft.ru>
2020-01-27 23:28:26 +0500

    FBCF-035. Подключил spring data

---
+++ src/main/java/ru/innopolis/service/impl/UserServiceImpl.java
@@ -0,0 +1,42 @@
+package ru.innopolis.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.innopolis.domain.User;
+import ru.innopolis.repository.UserRepository;
+import ru.innopolis.service.UserService;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+    @PersistenceContext
+    private EntityManager em;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).get();
+    }
+
+    @Override
+    public void delete(User user) {
+        userRepository.delete(user);
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+}
