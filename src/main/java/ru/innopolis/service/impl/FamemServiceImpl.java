402c8860bc0333255a72fd1b235b87c2deb4ebe9
ekaterina belolipetskaya <ia.kuzzia@gmail.com>
2020-02-01 00:27:57 +0300

    FBCF-027: Controller and services for updating personal info was updated.

--- src/main/java/ru/innopolis/service/impl/FamemServiceImpl.java
+++ src/main/java/ru/innopolis/service/impl/FamemServiceImpl.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.innopolis.domain.Famem;
+import ru.innopolis.domain.User;
 import ru.innopolis.repository.FamemRepository;
 import ru.innopolis.service.FamemService;
+import ru.innopolis.service.UserService;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -21,6 +23,13 @@ public class FamemServiceImpl implements FamemService {
 
     @Autowired
     private FamemRepository famemRepository;
+
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
 
     @Override
     public void save(Famem famem) {
@@ -44,5 +53,49 @@ public class FamemServiceImpl implements FamemService {
     public List<Famem> findAll() {
         log.info("find all famem");
         return famemRepository.findAll();
+    }
+
+    @Override
+    public Famem findByUserid(Long id) {
+        return famemRepository.findFirstByUserid(id);
+    }
+
+    /**
+     * Find famem with appropriate userId and returns it.
+     * In case famem is absent, create new famem instance with
+     * appropriate userId.
+     *
+     * @param id - userId, which is associated with the user and famem.
+     * @return - Famem object.
+     */
+    @Override
+    public Famem getByUserid(Long id) {
+        Famem famem = new Famem();
+        if (findByUserid(id) == null) {
+            famem.setUserid(id);
+            save(famem);
+            log.info("saved new family member associated with the user {}", id);
+        } else {
+            famem = findByUserid(id);
+            log.debug("get family member{}", famem);
+        }
+        return famem;
+    }
+
+    @Override
+    public void update(Famem newFamemInfo) {
+        Famem famem = getByUserid(newFamemInfo.getUserid());
+        famem.setDatebirth(newFamemInfo.getDatebirth());
+        famem.setName(newFamemInfo.getName());
+        famem.setPatronymic(newFamemInfo.getPatronymic());
+        famem.setSurname(newFamemInfo.getSurname());
+        save(famem);
+    }
+
+    @Override
+    public Famem getByLogin(String login) {
+        User user = userService.findFirstByLogin(login);
+        Long userId = user.getUserid();
+        return getByUserid(userId);
     }
 }
