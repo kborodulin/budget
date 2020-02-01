eee0290050d8020334caae057948f15fb67bc61d
Alex N <dobrijserv@gmail.com>
2020-02-01 20:48:24 +0300

    Revert "FBCF-021 Создал страницу/вкладку личного кабинета."

--- src/main/webapp/resources/js/personalAccount.js
+++ src/main/webapp/resources/js/personalAccount.js
@@ -1,9 +1,62 @@
 var setAccount = document.querySelectorAll(".set-control");
+var setFamily = document.querySelectorAll(".set-control-family");
+var setEditionFamily = document.querySelector("#edition-family");
+var setItemFamily = document.querySelector("#save-item-family")
+var setEditionNewFamily = document.querySelector("#edition-new-family");
+var setItemNewFamily = document.querySelector("#save-item-new-family")
 
-
-for (let input of setAccount)
-{
-    input.addEventListener("input",()=>{
-        document.querySelector("#save-item-account").style.display="";
-    })
+for (let input of setAccount) {
+    input.disabled = true;
 }
+for (let input of setFamily) {
+    input.disabled = true;
+}
+
+document.querySelector("#edition-account").addEventListener("click", e => {
+    e.preventDefault();
+    document.querySelector("#edition-account").style.display = "none";
+    document.querySelector("#save-item-account").style.display = "";
+    for (let input of setAccount) {
+        input.disabled = false;
+    }
+});
+
+document.querySelector("#save-item-account").addEventListener("click", e => {
+    e.preventDefault();
+    document.querySelector("#setingForm").submit()
+});
+
+if (setEditionFamily)
+    setEditionFamily.addEventListener("click", e => {
+    e.preventDefault();
+    document.querySelector("#edition-family").style.display = "none";
+    document.querySelector("#save-item-family").style.display = "";
+    document.querySelector("#nav-item-exit").style.display = "";
+    for (let input of setFamily) {
+        input.disabled = false;
+    }
+});
+if(setItemFamily)
+setItemFamily.addEventListener("click", e => {
+    e.preventDefault();
+    document.querySelector("#save-item-family").style.display = "none";
+    document.querySelector("#edition-family").style.display = "";
+    document.querySelector("#nav-item-exit").style.display = "none";
+    for (let input of setFamily) {
+        input.disabled = true;
+    }
+});
+if (setEditionNewFamily)
+setEditionNewFamily.addEventListener("click", e => {
+    e.preventDefault();
+    document.querySelector("#edition-new-family").style.display = "none";
+    document.querySelector("#save-item-new-family").style.display = "";
+    document.querySelector("#NewFamily").style.display = "";
+});
+if (setItemNewFamily)
+setItemNewFamily.addEventListener("click", e => {
+    e.preventDefault();
+    document.querySelector("#save-item-new-family").style.display = "none";
+    document.querySelector("#edition-new-family").style.display = "";
+    document.querySelector("#NewFamily").style.display = "none";
+});
