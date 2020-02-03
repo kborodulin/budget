33943df1261cccd0fcb0e209952391e2dd6b4d37
Alex N <dobrijserv@gmail.com>
2020-02-03 10:42:59 +0300

    добавил пункты меню

--- src/main/java/ru/innopolis/controller/AccountController.java
+++ src/main/java/ru/innopolis/controller/AccountController.java
@@ -73,6 +73,32 @@ public class AccountController {
         }
     }
 
+    @GetMapping(value = "/main")
+    public ModelAndView openMain(ModelAndView modelAndView) {
+        modelAndView.setViewName("/main");
+        return modelAndView;
+    }
+
+    @GetMapping(value = "/income")
+    public ModelAndView openIncome(ModelAndView modelAndView) {
+        modelAndView.setViewName("income");
+        return modelAndView;
+    }
+    @GetMapping(value = "/expenses")
+    public ModelAndView openExpenses(ModelAndView modelAndView) {
+        modelAndView.setViewName("expenses");
+        return modelAndView;
+    }
+    @GetMapping(value = "/wallet")
+    public ModelAndView openWallet(ModelAndView modelAndView) {
+        modelAndView.setViewName("wallet");
+        return modelAndView;
+    }
+    @GetMapping(value = "/statistic")
+    public ModelAndView openStatistic(ModelAndView modelAndView) {
+        modelAndView.setViewName("statistic");
+        return modelAndView;
+    }
     @InitBinder(value = "familyInfo")
     private void initBinder(WebDataBinder binder) {
         binder.setValidator(familyValidator);
