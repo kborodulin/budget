cd7c839118e2bbe16182277a5f36622c53ee1290
Alex <dobrijserv@gmail.com>
2020-02-08 17:43:29 +0300

    FBCF-302: Wallet creation added.

--- src/main/java/ru/innopolis/repository/FamemRepository.java
+++ src/main/java/ru/innopolis/repository/FamemRepository.java
@@ -4,7 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.innopolis.domain.Famem;
 
+import java.util.List;
+
 @Repository
 public interface FamemRepository extends JpaRepository<Famem, Long> {
     Famem findFirstByUserid(Long userid);
+
+    Famem findFirstByFamilyid(Long familyId);
+
+    List<Famem> findAllByFamilyid(Long familyId);
 }
