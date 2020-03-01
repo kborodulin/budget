2cceea6b47269f0be9ff97c4cde27c42f690c4b9
ekaterina belolipetskaya <ia.kuzzia@gmail.com>
2020-03-01 22:16:43 +0300

    FBCF-128: Добавлена пагинация на страницу статистики.

--- src/main/java/ru/innopolis/repository/OperationRepositoryCustom.java
+++ src/main/java/ru/innopolis/repository/OperationRepositoryCustom.java
@@ -8,17 +8,20 @@ import ru.innopolis.domain.Operation;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.persistence.criteria.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+
+import static ru.innopolis.controller.IncomeController.MAX_COUNT_ELEMENT_PAGE;
 
 @Repository
 public class OperationRepositoryCustom {
     @PersistenceContext
     private EntityManager entityManager;
 
-    public List<Operation> findAllOperationsBy(List<Famem> familyMembers, List<Category> categoryList, LocalDate startDate, LocalDate endDate, int operationType) {
+    public List<Operation> findAllOperationsBy(List<Famem> familyMembers, List<Category> categoryList, LocalDate startDate, LocalDate endDate, int operationType, Integer page) {
         CriteriaBuilder cb = entityManager.getCriteriaBuilder();
         CriteriaQuery<Operation> query = cb.createQuery(Operation.class);
 
@@ -42,9 +45,13 @@ public class OperationRepositoryCustom {
 
         query.select(operation)
                 .where(cb.and(predicates.toArray(new Predicate[0]))).orderBy(cb.asc(operation.get("dateoper")));
+        TypedQuery<Operation> operationTypedQuery = entityManager.createQuery(query);
+        if (page != null) {
+            operationTypedQuery.setFirstResult(page*MAX_COUNT_ELEMENT_PAGE);
+            operationTypedQuery.setMaxResults(MAX_COUNT_ELEMENT_PAGE);
+        }
 
-        return entityManager.createQuery(query)
-                .getResultList();
+        return operationTypedQuery.getResultList();
     }
 
     private Predicate getFamilyMemberPredicate(List<Famem> familyMembers, CriteriaBuilder cb, Path<Long> famemPath) {
