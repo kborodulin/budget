bba6d35fc37392d45976e5211d39d21341f3f2b3
Borodulin Kirill <kborodulin@diasoft.ru>
2020-03-25 18:32:19 +0500

    21

--- src/main/java/ru/innopolis/config/MessageSecurityWebApplicationInitializer.java
+++ src/main/java/ru/innopolis/config/MessageSecurityWebApplicationInitializer.java
@@ -6,4 +6,5 @@ import org.springframework.security.web.context.AbstractSecurityWebApplicationIn
 public class MessageSecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
 
 //
+    //
 }
